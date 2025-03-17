aa546062d27ced6cb480e03b0006b6aeae3ad229
jimmylu <mr.jimmylu@gmail.comn>
2025-03-17 18:56:49 +0800

    feature:add triggers

--- migrations/20250223105727_initial.sql
+++ migrations/20250223105727_initial.sql
@@ -59,3 +59,17 @@ CREATE INDEX IF NOT EXISTS idx_messages_chat_id_created_at ON messages (chat_id,
 
 -- create index for messages for sender_id
 CREATE INDEX IF NOT EXISTS idx_messages_sender_id ON messages (sender_id, created_at DESC);
+
+
+BEGIN;
+-- insert default user
+INSERT INTO users (id, fullname, email, ws_id, password_hash) VALUES (0, 'Default', 'default@default.com', 0, 'default');
+-- insert default workspace
+INSERT INTO workspaces (id, name, owner_id) VALUES (0, 'Default', 0);
+UPDATE users SET ws_id = 0 WHERE id = 0;
+INSERT INTO chats (id, name, type, members, ws_id) VALUES (0, 'Default', 'public_channel', ARRAY[0], 0);
+COMMIT;
+
+-- add foreign key constraint for ws_id for users
+ALTER TABLE users
+  ADD CONSTRAINT users_ws_id_fk FOREIGN KEY (ws_id) REFERENCES workspaces(id);
